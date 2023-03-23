795e94e1d7740dc6a84c886ed1ae716e89e7bbe3
PIYUSH <69582531+piyushraj1@users.noreply.github.com>
2023-03-23 10:19:16 +0530

    Add files via upload

---
+++ Day 10/scope_variables.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     int a=3;
+     cout<<a<<endl;
+
+     if(true){
+          int b = 5;
+          cout<< b <<endl;
+     }
+
+     int b=1;
+     cout<<b<<endl;
+
+     //int b= 3;
+
+     cout<< b<<endl;
+     int i = 8;
+     for (; i<8; i++){
+          cout<<"HI"<<endl;
+     }
+     if(1){
+          int b;
+          if(1){
+               int b;
+               if(1){
+                    int b;
+               }
+          }
+     }
+}
+
+//if a variable is declared in any block then it 
+//will be active in that block only and as we get out of that block it will give error
